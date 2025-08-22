6f85ccbd7e46d89fb346b8af9098dca64cf0cbef
oaitbenh <0xmrrandom@gmail.com>
2025-08-22 19:00:14 +0100

    some cases handled in parsing part && remove target files

--- target/debug/deps/Lexical-6b4b8789d95c7934.d
+++
@@ -1,5 +0,0 @@
-/home/oaitbenh/Documents/0-shell/target/debug/deps/Lexical-6b4b8789d95c7934.d: src/main.rs
-
-/home/oaitbenh/Documents/0-shell/target/debug/deps/Lexical-6b4b8789d95c7934: src/main.rs
-
-src/main.rs:
