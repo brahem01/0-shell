cc98a8c660e3b37e0c3b50fa02c5bd7e7b06b3bd
oaitbenh <0xmrrandom@gmail.com>
2025-08-22 18:59:38 +0100

    some cases handled in parsing part

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,62 @@
-pub enum Token {
-    Word(String),
-    Option(String),
-    Pipe,           // |
-    And,            // &&
-    Or,             // ||
-    Semicolon,      // ;
-    Background,     // &
-    RedirectIn,     // <
-    RedirectOut,    // >
-    RedirectAppend, // >>
-    Heredoc,        // <<
-    Assignment { var: String, value: String },
-    Variable(String),
-    CommandSubstitutionStart, // $(
-    CommandSubstitutionEnd,   // )
-    SingleQuoteStart,         // '
-    SingleQuoteEnd,           // '
-    DoubleQuoteStart,         // "
-    DoubleQuoteEnd,           // "
-    Comment(String),
-    Newline,
-}
-
+#[derive(PartialEq)]
 enum Status {
     Quotes(char),
-    Word()
+    Word,
     Operation(char),
     Null,
 }
 
-struct Tokens {
-    input: String(),
-    tokens: Vec<Token>,
+struct Code {
+    input: String,
+    tokens: Vec<String>,
 }
 
-impl Tokens {
-    fn new(input: &str) -> Tokens {
-        let mut tkns = Tokens {
+impl Code {
+    fn new(input: &str) -> Code {
+        let mut tkns = Code {
             input: input.to_string(),
             tokens: Vec::new(),
         };
         let mut status: Status = Status::Null;
-        let chars: Vec<char> = input.chars.collect::<Vec<char>>();
-        let mut index: usize = 0;
+        let chars: Vec<char> = input.chars().collect::<Vec<char>>();
         let mut word: String = String::new();
+	let input_len = input.len();
+        let mut index: usize = 0;
         while index < input.len() {
             match chars[index] {
-                "\"" | "'" => {
-                    if status == Status::Word(chars[index]) {
+                '"' | '\'' => {
+                    if matches!(status, Status::Quotes(c) if c == chars[index]) {
+			tkns.tokens.push(word);
+			status = Status::Null;
+			word = String::new();
                     } else if status == Status::Null {
+			status = Status::Quotes(chars[index]);
                     } else {
+			word.push(chars[index]);
                     }
                 }
+		'|' | '&' | '>' | '<' | ';' => {
+		    if matches!(status, Status::Quotes(_)) || matches!(status, Status::Operation(c) if c == chars[index]) {
+			word.push(chars[index]);
+		    } else if status != Status::Null {
+			tkns.tokens.push(word);
+			word = String::new();
+		    }
+		    if status == Status::Word || status == Status::Null {
+			status = Status::Operation(chars[index]);
+		    }
+		}
+		' ' if word.len() != 0 => {
+		    tkns.tokens.push(word);
+		}
+		_=> {
+		    tkns.tokens.push(word);
+		}
             }
+	    index += 1;
         }
+	println!("Tokens: {:?}", tkns.tokens);
+	tkns
     }
 }
 
