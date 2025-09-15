470ecee8c973eb05240f8d2cc7d50402ea32951f
ielharra <moroccobrahem@gmail.com>
2025-09-15 12:02:32 +0100

    remove the repititive error in cd

--- commands/src/builtin/cd.rs
+++ commands/src/builtin/cd.rs
@@ -10,7 +10,6 @@ impl Command for Cd {
     fn run(&self, cmd:  &mut Cmd) {
         if let Some(path) = cmd.args.get(0) {
             if let Err(e) = env::set_current_dir(path) {
-                cmd.stderr.write_all(format!("cd: {}: {}\n", path, e).as_bytes()).unwrap();
                 let _ = writeln!(cmd.stderr, "cd: {}: {}", path, e);
             }
         } else {
